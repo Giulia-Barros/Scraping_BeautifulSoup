f5dc8aa4f9cbdfabc053a02b541ef44f2f2700ce
Giulia-Barros <130318382+Giulia-Barros@users.noreply.github.com>
2025-06-01 06:56:44 -0300

    atualizacaoComentarios

--- interface.py
+++ interface.py
@@ -1,8 +1,20 @@
+# /***********************************************************************************
+# |----------------------------------------------------------------------------------|
+# |* Programa   | Interface                                                         *|
+# |----------------------------------------------------------------------------------|
+# |* Autor      |  Giulia Barros                                                    *|
+# |----------------------------------------------------------------------------------|
+# |* Utilização | Execução e visualização do programa Quotes                        *|
+# |----------------------------------------------------------------------------------|
+# |* Descricao  | Interface para executar e visualizar os resultados do programa    *|
+# |             | Quetos.                                                            |
+# |----------------------------------------------------------------------------------|
+
 import customtkinter as ctk
 import threading
 from quotes import analisa_citacoes
 
-
+# Executa a raspagem e limpa toda área de texto da interface
 def executarRaspagem():
     resultados.delete('0.0', 'end')
     citacoes = analisa_citacoes()
@@ -11,21 +23,24 @@ def executarRaspagem():
         texto = f"Autor: {c['Nome do autor']}\nFrase: {c['Citação']}\nTags: {', '.join(c['Tags'])}\n---\n"
         resultados.insert("end", texto)
 
+# Executa em segundo plano para não travar a interface
 def chamar_thread():
     threading.Thread(target=executarRaspagem).start()
 
-#Configurando a aparência
+# Configurando a aparência
+
 ctk.set_appearance_mode('dark')
-#Criando Janela
+
+# Criando janela da interface
 interface = ctk.CTk()
 interface.title('Raspagem de Dados - Quotes to Scrape')
 interface.geometry('700x500')
 
-#Criando Botao
+# Botão para excutar a raspagem do programa Quetos
 botao_execucao = ctk.CTkButton(interface, text='Excutar', command=chamar_thread)
 botao_execucao.pack(pady=10)
 
-#Criando Área de Texto
+# Área de texto, onde os dados serão exibidos
 resultados = ctk.CTkTextbox(interface, width=600, height=300)
 resultados.pack(pady=20)
 
